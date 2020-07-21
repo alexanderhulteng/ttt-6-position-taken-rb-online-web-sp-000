057850571323030a9a48332585ada80eac7082d9
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-21 00:21:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
-#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-#index = 1
+
 def position_taken?(board, index)
  if board[index] == " "
+   false
+ elsif board[index] == ""
    false
  end
 end
