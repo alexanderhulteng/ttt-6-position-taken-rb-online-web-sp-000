67e1fc4e2f30bb6a15e2ae6b509059630cccb87d
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-21 00:24:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 
 def position_taken?(board, index)
- if board[index] == " " 
+ if board[index] == " " || board[index] == "" || board[index] == nil
    false
- elsif board[index] == ""
-   false
- elsif board[index] == nil
-   false
+ 
+ 
  elsif board[index] == "X" || board[index] == "O"
    true
 
