3ff6456effc1d3eaeb6a2d79241835132591a32e
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-21 00:16:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,7 @@
-board = [" ", " ", " ", "f", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
  if board[index] == " "
    puts "false"
  end
 end
-
